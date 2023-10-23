033ef8431343d30583d6d5c725c46bbbdbc3619c
ik012982i9 <aduronco23dw@ikzubirimanteo.com>
2023-10-23 13:19:38 +0200

    DESARROLLADO PRIMO

--- src/src/Main.java
+++ src/src/Main.java
@@ -9,6 +9,31 @@ public class Main {
             //ver el string es palindromo
             //[
             //ver si el numero es primo
+            public class NumeroPrimo {
+                public static void main(String[] args) {
+                    int NUM = 0; // Cambia este número por el que desees verificar
+
+                    boolean esPrimo = true; // Suponemos inicialmente que el número es primo
+
+                    if (NUM <= 1) {
+                        esPrimo = false; // Los números menores o iguales a 1 no son primos
+                    } else {
+                        for (int i = 2; i <= NUM; i++) {
+                            if (NUM % i == 0) {
+                                esPrimo = false; // Si es divisible por algún número distinto de 1 y sí mismo, no es primo
+                                break; // Salimos del bucle
+                            }
+                        }
+                    }
+
+                    if (esPrimo) {
+                        System.out.println(numero + " es un número primo.");
+                    } else {
+                        System.out.println(numero + " no es un número primo.");
+                    }
+                }
+            }
+
 
             //ver cuantas mayusculas tiene la cadena
 

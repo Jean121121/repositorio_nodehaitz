e267c3f9b93b0bac5842330380c82622bc00525c
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-23 13:45:48 +0200

    Añadir tamaño cadena

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         {
             int numero;
             String cadena;
+            Scanner sc= new Scanner(System.in);
+            System.out.print("Introduce una palabra jeje: ");
+            cadena=sc.next();
+
             //con el scaner cogeis un numero entero positivo y una cadena
 
             //ver el string es palitroque
-            //[
+
             //ver si el numero es primo
 
             //ver cuantas mayusculas tiene la cadena
@@ -15,6 +21,7 @@ public class Main {
             //Si no es primo factorizarlo:
 
             //mostrar el tamaño de la cadena
+            System.out.println("La palabra "+cadena+" tiene "+cadena.length()+" letras.");
         }
     }
 }
